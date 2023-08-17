2233ed10b456198f1523e7355906f7fe5101f925
noea7 <114760692+noea7@users.noreply.github.com>
2023-08-17 13:41:04 +0300

    Back end initial commit

---
+++ booking-api/src/main/java/lt/techin/kristina/bookingapi/BookingApiApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.kristina.bookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApiApplication.class, args);
+	}
+
+}
---
+++ booking-api/src/test/java/lt/techin/kristina/bookingapi/BookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.kristina.bookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
